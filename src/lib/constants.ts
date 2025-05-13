6627304086d776ec1ec802bd8faa3a81c96fae4c
loothero <100039621+loothero@users.noreply.github.com>
2025-05-13 16:06:41 -0400

    update quest -> settings map

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
 export const NAMESPACE = "ds_v1_2_0";
 export const ACTIONS_MAP = {
-  1: "Completed Easy Mode",
-  2: "Completed Medium Mode",
-  3: "Completed Hard Mode",
-  10: "Completed Nightmare Mode",
+  3: "Completed Newbie Mode",
+  6: "Completed Easy Mode",
+  4: "Completed Medium Mode",
+  1: "Completed Hard Mode",
+  5: "Completed Nightmare Mode",
 };
